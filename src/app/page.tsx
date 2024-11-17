8428c7841a79769c981e8d1c365bc94f6d632678
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-11-17 12:23:55 +0500

    card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,58 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col mt-10 mx-5 my-20">
+    <h1 className="text-4xl my-5 flex items-center justify-center">Governor Sindh Cards</h1>
+    <div className="flex flex-wrap justify-between mt-10 m-3 sm:m-0 sm:gap-0 gap-3">
+      
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          className="shadow-2xl shadow-black rounded-lg h-[300px] object-cover sm:w-[32%] w-full"
+          src="/images/card1.jpg"
+          alt="Card 1"
+         width="1088" height="896"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      
+      
+        <Image
+          className="shadow-2xl shadow-black rounded-lg h-[300px] object-cover sm:w-[32%] w-full"
+          src="/images/card2.jpg"
+          alt="Card 2"
+         width="1088" height="896"
+        />
+      
+      
+        <Image
+          className="shadow-2xl shadow-black rounded-lg h-[300px] object-cover sm:w-[32%] w-full"
+          src="/images/card3.jpg"
+          alt="Card 3"
+         width="1088" height="896"
+        />
+      
     </div>
+
+    <h1 className="text-4xl my-5 flex items-center justify-center mt-10">Governor Sindh Cards</h1>
+    <div className="flex flex-wrap justify-between mt-10 m-3 sm:m-0 sm:gap-0 gap-3">  
+        <Image
+          className="shadow-2xl shadow-black rounded-lg h-[300px] object-cover sm:w-[50%] w-full"
+          src="/images/card2.jpg"
+          alt="Card 2"
+         width="1088" height="896"
+        />
+      
+      
+        <Image
+          className="shadow-2xl shadow-black rounded-lg h-[300px] object-cover sm:w-[50%] w-full"
+          src="/images/card3.jpg"
+          alt="Card 3"
+         width="1088" height="896"
+        />
+      
+    </div>
+  </div>
+  
   );
 }
+{/* <div className=" bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+  <Image className="w-full h-auto rounded-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Card Image"/>
+ 
+</div> */}
